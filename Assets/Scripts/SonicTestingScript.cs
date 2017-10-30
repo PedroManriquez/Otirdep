9f9bcb02d7d3489ddf48efec7f51c5316237a26d
MrSenk <camilo.c_n@hotmail.com>
2017-10-30 14:40:53 -0300

    jeje
    jejejeje, sonic por mientras, reemplazar con personaje real

---
+++ Assets/Scripts/SonicTestingScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonicTestingScript : MonoBehaviour {
+
+	public float velocidadMax = 10f;
+	bool mirandoDerecha = true;
+	Rigidbody2D rigi;
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		rigi = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float move = Input.GetAxis ("Horizontal");
+
+		anim.SetFloat ("Velocidad", Mathf.Abs (move));
+
+		rigi.velocity = new Vector2 (move * velocidadMax, rigi.velocity.y);
+
+		if (move > 0 && !mirandoDerecha) {
+			GetComponent<SpriteRenderer> ().flipX = false;
+			mirandoDerecha = true;
+		} else if (move < 0 && mirandoDerecha) {
+			GetComponent<SpriteRenderer> ().flipX = true;
+			mirandoDerecha = false;
+		}
+
+	}
+
+	void Girar(){
+		mirandoDerecha = !mirandoDerecha;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
